69f5c13464242a6936ae8513cfed12408cef0baa
KalliPutt <kalli.putt.5.23.07@gmail.com>
2025-03-27 10:37:41 -0400

    Started on the account page

--- main.py
+++ main.py
@@ -1,19 +1,20 @@
 from flask import Flask, render_template, request
 from sqlalchemy import create_engine, text
 
-app = Flask(__name__) #Initialize the Flask app
+app = Flask(__name__) 
 
-# Database connection string (update credentials as needed)
-# Formating: mysql://user:password@server/database
-con_str = "mysql://root:cset155@localhost/boatdb"
+con_str = "mysql://root:cset155@localhost/testing"
 engine = create_engine(con_str, echo=True)
-conn = engine.connect() #creates the database engine
+conn = engine.connect() 
 
 # Home route - displays the home page
 @app.route('/')
 def hello():
     return render_template('base.html')
 
+@app.route('/accounts')
+def accounts():
+    return render_template('accounts.html')
 
 # Run the Flask application in debug mode
 if __name__ == '__main__':
